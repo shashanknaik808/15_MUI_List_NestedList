c6c72c8686832693c1101dc47e736d6a3a89317c
Shashank Naik <shashanknaik808@gmail.com>
2023-12-05 16:19:22 +0530

    Implemented a collapsible List with an expandable item and sub-items

--- mui-tut/src/Test.jsx
+++ mui-tut/src/Test.jsx
@@ -2,7 +2,31 @@ import React from 'react';
 
 function Test() {
     return (
-        <div>Test</div>
+        <div>
+            <Box>
+                <List>
+                    <ListItem divider>
+                        <ListItemButton onClick={() => setOpen(true)}>
+                            <ListItemIcon>{">"}</ListItemIcon>
+                            <ListItemText primary={"Expand List"} />
+                        </ListItemButton>
+                    </ListItem>
+                </List>
+
+                <Collapse in={open}>
+                    <List sx={{ marginLeft: 25 }}>
+                        {array.map((listElm) => (
+                            <ListItem divider>
+                                <ListItemButton onClick={() => setOpen(false)}>
+                                    <ListItemText primary={listElm} />
+                                </ListItemButton>
+                            </ListItem>
+                        ))}
+                    </List>
+                </Collapse>
+
+            </Box>
+        </div>
     )
 }
 
